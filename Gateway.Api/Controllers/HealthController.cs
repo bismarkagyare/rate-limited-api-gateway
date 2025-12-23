8a0af7ab3f2a12c5fdadd7d46dc3357171809988
Bismark Agyare <bismarkagyare54@gmail.com>
2025-12-23 12:37:48 +0000

    add health controller

---
+++ Gateway.Api/Controllers/HealthController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("health")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new { status = "healthy" });
+    }
+}
