6914d2e6a8159ea8b57e22cfe74d5979b6ad384b
Bismark Agyare <bismarkagyare54@gmail.com>
2025-12-30 12:47:11 +0000

    update error messages with invalid key

--- Gateway.Api/Common/Errors/ErrorMessages.cs
+++ Gateway.Api/Common/Errors/ErrorMessages.cs
@@ -4,5 +4,6 @@ public static class ErrorMessages
 {
     public const string MissingApiKey = "API key is missing";
     public const string InvalidApiKey = "Invalid API key";
+    public const string ApiKeyDisabled = "API key is disabled.";
     public const string RateLimitExceeded = "Rate limit exceeded. Try again later.";
 }
